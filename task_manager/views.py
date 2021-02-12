9f4e306d8572b7981eac927d57907f9eafda2dfd
Morteza Seydi <mortoozseydi@gmail.com>
2021-02-12 18:04:08 +0330

    added boards.html and template inheritance ready for models.

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'boards.html')
+        data = {"user": request.user,
+                "first": request.user.username[0]
+                }
+        return render(request, 'boards.html', data)
     else:
         return redirect('signIn')
+
+
